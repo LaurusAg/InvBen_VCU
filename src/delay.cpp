161fdffa078085211c95bf036901fd609440878f
Lautaro-Laurus <lautaro@laurusag.tech>
2023-07-18 12:25:09 -0300

    added stop functionality

--- src/delay.cpp
+++ src/delay.cpp
@@ -5,6 +5,7 @@ Delay::Delay()
 {
     start_time = 0;
     interval_time = 0;
+    stopped = false;
 }
 
 /**
@@ -16,6 +17,7 @@ void Delay::start(unsigned long interval)
 {
     start_time = millis();
     interval_time = interval;
+    stopped = false;
 }
 /**
  * @brief Checks if interval of time has expired.
@@ -35,3 +37,12 @@ void Delay::reset()
     start_time = millis();
 }
 
+/**
+ * @brief Change the status of stopped variable. Ends a timer in order to use it just one time.
+ * 
+ * @return true or false. 
+ */
+void Delay::stop()
+{
+    stopped = true; 
+}
